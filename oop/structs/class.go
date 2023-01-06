8ef13afb5b3f7caa68df040929f1946e1dd570b2
Bruno <brunoveranoc@gmail.com>
2023-01-06 09:27:29 -0500

    Class 02: Methods and Functions

--- oop/structs/class.go
+++ oop/structs/class.go
@@ -8,6 +8,22 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
 	fmt.Printf("%v", e)
@@ -15,4 +31,11 @@ func main() {
 	e.id = 1
 	e.name = "Bruno"
 	fmt.Printf("%v", e)
+
+	e.SetId(5)
+	e.SetName("Alonso")
+	fmt.Printf("%v", e)
+
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }
