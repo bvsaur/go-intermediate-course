059e9d61fbffd72766d58479ab2b49898b0c08b9
Bruno <brunoveranoc@gmail.com>
2023-01-06 12:40:00 -0500

    Class 06: Abstract Factory Desing Pattern

---
+++ factory/main.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+// SMS and Email notifications
+
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending notification via SMS")
+}
+
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+type SMSNotificationSender struct {
+}
+
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
+}
+
+type EmailNotification struct {
+}
+
+func (EmailNotification) SendNotification() {
+	fmt.Println("Sending notification via Email")
+}
+
+type EmailNotificationSender struct {
+}
+
+func (EmailNotificationSender) GetSenderMethod() string {
+	return "Email"
+}
+
+func (EmailNotificationSender) GetSenderChannel() string {
+	return "Mailgun"
+}
+
+func (EmailNotification) GetSender() ISender {
+	return EmailNotificationSender{}
+}
+
+func getNotificationFactory(notificationType string) (INotificationFactory, error) {
+	switch notificationType {
+	case "SMS":
+		return SMSNotification{}, nil
+	case "Email":
+		return EmailNotification{}, nil
+	default:
+		return nil, fmt.Errorf("No notification type")
+	}
+}
+
+func sendNotification(f INotificationFactory) {
+	f.SendNotification()
+}
+
+func getMethod(f INotificationFactory) {
+	fmt.Printf("Method: %s\n", f.GetSender().GetSenderMethod())
+}
+
+func main() {
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+
+	sendNotification(smsFactory)
+	sendNotification(emailFactory)
+
+	getMethod(smsFactory)
+	getMethod(emailFactory)
+}
